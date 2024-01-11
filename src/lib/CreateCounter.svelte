e845f97e4f108192f6c84efc5219928c0a61774e
motsu8e <motsu159357@gmail.com>
2024-01-12 00:06:58 +0900

    全タイトル表示 #12

--- src/lib/CreateCounter.svelte
+++ src/lib/CreateCounter.svelte
@@ -22,9 +22,17 @@
 		counters = counters.filter(({ id }) => id !== e.detail.id);
 	};
 
+	/**
+	 * reactive
+	 */
 	$: reduceCount = counters.reduce((total, current) => {
 		return total + current.count;
 	}, 0);
+
+	$: allTitle = counters.reduce((string, current, index) => {
+		if (index === counters.length - 1) return `${string}${current.title}`;
+		else return `${string}${current.title}, `;
+	}, '');
 </script>
 
 <div class="space-y-2 py-2">
@@ -39,9 +47,7 @@
 
 	<button on:click={createCounter}> Add Counter </button>
 
-	{#each counters as counter}
-		<p>{counter.title}</p>
-	{/each}
+	<p>title list: {allTitle}</p>
 
 	<p>sum of count: {reduceCount}</p>
 </div>
