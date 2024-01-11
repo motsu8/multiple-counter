ce3b7ac828728879e39861d815716518a59e000b
motsu8e <motsu159357@gmail.com>
2024-01-11 21:02:48 +0900

    モックアップ作成

---
+++ src/lib/Counter.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+	export let count: number = 0;
+  export let title: string
+	const increment = () => (count = count + 1);
+	const decrement = () => (count = count - 1);
+	const reset = () => count = 0;
+</script>
+
+<div>
+  <input type="text" bind:value={title}>
+  <p>{count}</p>
+  <button on:click={increment}>+</button>
+  <button on:click={decrement}>-</button>
+  <button on:click={reset}>0</button>
+</div>
+
+<style>
+  div{
+    background-color: antiquewhite;
+  }
+</style>
