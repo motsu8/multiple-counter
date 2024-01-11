a6e941ebea99612b29b9df9ce637d928420f1d32
motsu8e <motsu159357@gmail.com>
2024-01-11 21:31:27 +0900

    tailwindcssの導入 #5

--- src/lib/Counter.svelte
+++ src/lib/Counter.svelte
@@ -1,21 +1,17 @@
 <script lang="ts">
 	export let count: number = 0;
-  export let title: string
+	export let title: string;
 	const increment = () => (count = count + 1);
 	const decrement = () => (count = count - 1);
-	const reset = () => count = 0;
+	const reset = () => (count = 0);
 </script>
 
-<div>
-  <input type="text" bind:value={title}>
-  <p>{count}</p>
-  <button on:click={increment}>+</button>
-  <button on:click={decrement}>-</button>
-  <button on:click={reset}>0</button>
+<div class="bg-neutral-100 px-3 py-2 flex space-x-3">
+	<input type="text" bind:value={title} class="" />
+	<p>{count}</p>
+	<div>
+		<button on:click={increment}>+</button>
+		<button on:click={decrement}>-</button>
+		<button on:click={reset}>0</button>
+	</div>
 </div>
-
-<style>
-  div{
-    background-color: antiquewhite;
-  }
-</style>
